b81f1e2a63e05da85ce5bdd33822e44c42470fce
Fernando Uval <fernandouval@gmail.com>
2021-06-06 22:44:32 -0300

    Funcionalidad básica en Ionic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,24 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  converted: number;
+  original: number;
+  unit: string = 'usd';
+  factor = {
+    'usd': 1,
+    'antel-arena': 10,
+    'tele-comunicaciones': 20,
+    'otro' : 100
+  }
   constructor() {}
+
+  dataChanged(origin: string) {
+    if (origin == 'converted') {
+      this.original = this.factor[this.unit]*this.converted;
+    }
+    else {
+      this.converted = this.factor[this.unit]*this.original;
+    }
+  }
 
 }
